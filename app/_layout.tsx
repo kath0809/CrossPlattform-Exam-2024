64667e516c8b89f217d9726fcaf5dd94fa309c8c
kath0809 <kath014@student.kristiania.no>
2024-11-01 23:59:19 +0100

    Added Tailwind/Nativewind to project

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,13 @@
 import { Stack } from "expo-router";
+import { Slot } from "expo-router";
+
+// Import your global CSS file
+import "../global.css";
 
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="index" />
+      <Slot />
     </Stack>
   );
 }
