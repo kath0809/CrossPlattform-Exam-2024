bb54b500c5e7ca560436889324fcd85482853d71
kath0809 <kath014@student.kristiania.no>
2024-11-11 20:11:53 +0100

    Fixed to only show urls

--- app/(tabs)/gallery.tsx
+++ app/(tabs)/gallery.tsx
@@ -31,12 +31,9 @@ export default function Gallery() {
         showsHorizontalScrollIndicator={false}
         style={styles.carousel}
       >
-        
-        {item.imageURLs && item.imageURLs.length > 0
-          ? item.imageURLs.map((uri, index) => (
-              <Image key={index} source={{ uri }} style={styles.image} />
-            ))
-          : null}
+        {item.imageURLs.map((image) => (
+          <Image key={image} source={{ uri: image }} style={styles.image} />
+        ))}
       </ScrollView>
 
       <Text style={styles.title}>{item.title}</Text>
