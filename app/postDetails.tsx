239838c6095b7a271a0fefe2277996066b46677e
kath0809 <kath014@student.kristiania.no>
2024-11-14 04:03:46 +0100

    Added like func to posts

--- app/postDetails.tsx
+++ app/postDetails.tsx
@@ -213,20 +213,37 @@
 //   },
 // });
 
-
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import {
-  Text,
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
+import {
   Image,
   ActivityIndicator,
   Alert,
   ScrollView,
-  Button,
+  StyleSheet,
+  Pressable,
+  TextInput,
+  View,
+  Text,
+  Dimensions,
 } from "react-native";
-import { PostData } from "@/utils/postData";
+import { CommentObject, PostData } from "@/utils/postData";
 import * as postApi from "@/api/postApi";
 import { useLocalSearchParams, useRouter } from "expo-router";
-import { useAuth } from "@/providers/authContext"; // Adjust the import path as needed
+import { useAuth } from "@/providers/authContext";
+import MapView, { Marker } from "react-native-maps";
+import { AntDesign, Ionicons } from "@expo/vector-icons";
+import LoadingComponent from "@/components/LoadingComponent";
+
+type PostProps = {
+  postData: PostData;
+  toggleLike: (id: string) => void;
+};
+
+const { width } = Dimensions.get("window");
 
 export default function PostDetail() {
   const { postId } = useLocalSearchParams();
@@ -234,15 +251,20 @@ export default function PostDetail() {
   const [post, setPost] = useState<PostData | null>(null);
   const [loading, setLoading] = useState(true);
   const router = useRouter();
+  const [loadingAddComment] = useState(false);
+  const [postComments, setPostComments] = useState<CommentObject[]>([]);
+  const [newComment, setNewComment] = useState("");
 
-  
+  const [isLiked, setIsLiked] = useState(false);
+  const [numLikes, setNumLikes] = useState(0);
+
   useEffect(() => {
     const fetchPost = async () => {
       try {
         if (postId) {
           const fetchedPost = await postApi.getPostById(
             Array.isArray(postId) ? postId[0] : postId
-          ); // Fetch post by ID
+          );
           setPost(fetchedPost);
         }
       } catch (error) {
@@ -257,57 +279,200 @@ export default function PostDetail() {
         "Access Denied",
         "You need to be signed in to view this post."
       );
-      router.push("/signIn"); // Redirect to sign-in page
+      router.push("/signIn");
     } else {
       fetchPost();
     }
   }, [postId, isAuthenticated, user]);
 
-  if (loading) {
-    return <ActivityIndicator size="large" />;
-  }
+  useEffect(() => {
+    if (post) {
+      setIsLiked(post.likes?.includes(user?.uid ?? "") ?? false);
+      setNumLikes(post.likes?.length ?? 0);
+    }
+  }, [post, user]);
 
-  if (!post) {
-    return <Text>Post not found.</Text>;
-  }
+  const handleLikePress = async () => {
+    if (post) {
+      if (isLiked) {
+        setNumLikes(numLikes - 1);
+        setIsLiked(false);
+      } else {
+        setNumLikes(numLikes + 1);
+        setIsLiked(true);
+      }
+      await postApi.toggleLikePost(post.id, user?.uid ?? "");
+    }
+  };
 
   return (
-    <ScrollView>
-      <Text>{post.title}</Text>
-      <Text>{post.description}</Text>
-      <Text>
-        {post.postCoordinates
-          ? `${post.postCoordinates.latitude}, ${post.postCoordinates.longitude}`
-          : "Coordinates not available"}
-      </Text>
-      <Text>{post.category}</Text>
-      {/* Display images */}
-      {post.imageURLs.map((image, index) => (
-        <Image
-          key={index}
-          source={{ uri: image }}
-          style={{ width: 200, height: 200 }}
-        />
-      ))}
-      {/* Add comments and likes functionality */}
-      <Button
-        title="Like"
-        onPress={() => {
-          /* Implement like functionality */
-        }}
-      />
-      <Button
-        title="Comment"
-        onPress={() => {
-          /* Implement comment functionality */
-        }}
-      />
-      <Button
-        title="Go back"
-        onPress={() => router.push("/(tabs)/gallery")}
-      ></Button>
-    </ScrollView>
+    <View style={{ paddingTop: hp(8) }} className="flex-1 justify-center gap-6">
+      <ScrollView
+        keyboardDismissMode="interactive"
+        automaticallyAdjustKeyboardInsets
+        contentContainerStyle={{ flexGrow: 1 }}
+      >
+        <View style={{ position: "relative" }}>
+          <Pressable
+            onPress={() => router.push("/(tabs)/gallery")}
+            style={{ left: 16, zIndex: 10, paddingBottom: 8 }}
+          >
+            <Ionicons name="arrow-back" size={24} color="black" />
+          </Pressable>
+          <ScrollView
+            horizontal
+            pagingEnabled
+            showsHorizontalScrollIndicator={false}
+            style={styles.carousel}
+          >
+            {post?.imageURLs.map((imageURL, index) => (
+              <Image
+                key={index}
+                source={{ uri: imageURL }}
+                style={styles.image}
+                resizeMode="cover"
+              />
+            ))}
+          </ScrollView>
+          <View className="flex-row items-center px-2">
+            <View className="flex-1 flex-row justify-center">
+              {post?.imageURLs && post.imageURLs.length > 1 && (
+                <View className="flex-row justify-center py-2">
+                  {post.imageURLs.map((_, index) => (
+                    <View
+                      key={index}
+                      className="w-2 h-2 rounded-full bg-gray-400 mx-1"
+                    />
+                  ))}
+                </View>
+              )}
+            </View>
+            <View className="flex-row items-center justify-end">
+              <Pressable
+                onPress={handleLikePress}
+                className="flex-row items-center"
+              >
+                {isLiked ? (
+                <Ionicons
+                  name="heart"
+                  size={30}
+                  color={isLiked ? "#f5a442" : "black"}
+                />
+                ) : (
+                <Ionicons
+                  name="heart-outline"
+                  size={30}
+                  color={isLiked ? "#f5a442" : "black"}
+                />
+                )}
+                <Text className="text-lg text-gray-600 px-1">{numLikes}</Text>
+              </Pressable>
+            </View>
+          </View>
+        </View>
+        <View className="px-4">
+          <Text className="text-2xl font-bold mb-2">{post?.title}</Text>
+          <Text className="text-base text-gray-700 mb-2">
+            {post?.description}
+          </Text>
+          <View className="mt-1">
+            <Text className="text-lg font-bold mb-2">Comments</Text>
+            {loading ? (
+              <LoadingComponent size={30} />
+            ) : (
+              postComments.map((comment) => (
+                <View
+                  key={comment.id}
+                  className="flex-row justify-between items-center mb-2"
+                >
+                  <View className="flex-row gap-1">
+                    <Text className="text-gray-700 font-semibold">
+                      {comment.comment.authorName}:
+                    </Text>
+                    <Text className="text-gray-600">
+                      {comment.comment.comment}
+                    </Text>
+                  </View>
+                  {comment.comment.authorId === user?.uid && (
+                    <Pressable
+                      onPress={() => {
+                        setPostComments(
+                          postComments.filter((c) => c.id !== comment.id)
+                        );
+                      }}
+                    >
+                      <Text className="text-red-500">Delete</Text>
+                    </Pressable>
+                  )}
+                </View>
+              ))
+            )}
+
+            
+            <View className="flex-row items-center mt-2">
+              <TextInput
+                value={newComment}
+                onChangeText={setNewComment}
+                placeholder="Add a comment..."
+                className="flex-1 border border-gray-300 rounded-md px-2 py-1"
+              />
+              <Pressable className="ml-2 bg-custom-orange rounded-md px-4 py-2">
+                {loadingAddComment ? (
+                  <LoadingComponent size={wp(25)} />
+                ) : (
+                  <Text className="text-black font-bold">Post</Text>
+                )}
+              </Pressable>
+            </View>
+          </View>
+          {/* Map View */}
+          {post && (
+            <Pressable onPress={() => router.push("/(tabs)/map")}
+            className="mt-8 rounded-lg overflow-hidden">
+              <MapView
+                initialRegion={{
+                  latitude: post?.postCoordinates?.latitude ?? 0,
+                  longitude: post?.postCoordinates?.longitude ?? 0,
+                  latitudeDelta: 0.0082,
+                  longitudeDelta: 0.0081,
+                }}
+                style={{ width: "100%", height: 200 }}
+              >
+                <Marker
+                  coordinate={{
+                    latitude: post?.postCoordinates?.latitude ?? 0,
+                    longitude: post?.postCoordinates?.longitude ?? 0,
+                  }}
+                  title={post?.title}
+                />
+              </MapView>
+             </Pressable>
+          )}
+        </View>
+      </ScrollView>
+    </View>
   );
 }
 
-
+const styles = StyleSheet.create({
+  carousel: {
+    width: width,
+    height: width * 0.6, // Reduce the height of the carousel
+  },
+  image: {
+    width: width,
+    height: width * 0.6, // Matches the reduced height of the carousel
+  },
+  dotContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    paddingVertical: 8,
+  },
+  dot: {
+    width: 8,
+    height: 8,
+    borderRadius: 4,
+    backgroundColor: "#bbb",
+    marginHorizontal: 4,
+  },
+});
